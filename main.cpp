185ac16c88dc6a6dfff432fe8be272dd7a797fb8
bartoszk97 <32266617+bartoszk97@users.noreply.github.com>
2017-10-23 14:51:26 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+using namespace std;
+int main(int argc, char* argv[])
+{
+    double x1, x2;
+    string linia;
+    typedef pair <double,double> Probka;
+    ifstream plik("sygnal.csv");
+    vector <Probka> dane;
+    while (getline(plik, linia))
+    {
+        stringstream ss(linia);
+        ss >> x1;
+        ss.ignore();
+        ss >> x2;
+        dane.push_back(Probka(x1, x2));
+    }
+    plik.close();
+    for (int i=1; i<dane.size(); i++)
+    {
+        cout << dane[i].first << "," << dane[i].second << endl;
+    }
+    ofstream plik2 ("out.csv", ios::app);
+    for (int i=1; i<dane.size(); i++)
+        plik2 << dane[i].first << dane[i].second << endl;
+    plik.close();
+
+
+
+}
