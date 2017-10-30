08fc3b00b0a3188990e0d9f9c2356243af5e5dcd
bartoszk97 <32266617+bartoszk97@users.noreply.github.com>
2017-10-30 15:36:24 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+
 using namespace std;
 int main(int argc, char* argv[])
+
+struct Probka
 {
     double x1, x2;
     string linia;
@@ -18,18 +21,78 @@ int main(int argc, char* argv[])
         ss >> x1;
         ss.ignore();
         ss >> x2;
-        dane.push_back(Probka(x1, x2));
+dane.push_back(Probka(x1, x2));
+
     }
     plik.close();
-    for (int i=1; i<dane.size(); i++)
-    {
-        cout << dane[i].first << "," << dane[i].second << endl;
-    }
-    ofstream plik2 ("out.csv", ios::app);
-    for (int i=1; i<dane.size(); i++)
-        plik2 << dane[i].first << dane[i].second << endl;
+    return dane;
+}
+
+void wyswietl (const vector<Probka>& dane)
+{
+    for (int i = 0; i < dane.size(); ++i)
+        {
+            cout << dane[i].t << ", " << dane[i].x << endl;
+        }
+}
+
+void zapisz (const vector<Probka>& dane, const string& nazwa_pliku)
+{
+    ofstream plik;
+    plik.open(nazwa_pliku.c_str(), ios::out);
+    for (int i = 0; i < dane.size(); ++i)
+        {
+            plik << dane[i].t << ", " << dane[i].x << endl;
+        }
     plik.close();
+}
 
 
 
+double maksimum(const vector <Probka>& dane)
+{
+    double _max = -1000000;
+    for ( int i=0; i<dane.size(); i++)
+        {
+            if (dane[i].x > _max)
+            _max = dane[i].x;
+        }
+    return _max;
+}
+
+double minimum(const vector <Probka>& dane)
+{
+    double _min = 10000000;
+    for ( int i=0; i<dane.size(); i++)
+        {
+            if (dane[i].x < _min)
+                _min = dane[i].x;
+        }
+    return _min;
+}
+
+double srednia (const vector<Probka>& dane)
+{
+    double m = 0.0;
+    for (int i = 0; i < dane.size(); ++i)
+        {
+            m += dane[i].x;
+        }
+    if (dane.size() > 0)
+        {
+            m = m / dane.size();
+        }
+    return m;
+}
+
+int main ()
+{
+    string o_nazwa, z_nazwa;
+    cout << "Plik odczytowy(-.csv) : "; cin >> o_nazwa;
+    cout << "Plik zapisowy(-.csv) : "; cin >> z_nazwa;
+    wyswietl (wczytaj(o_nazwa));
+    cout << "Maksimum : " << maksimum(wczytaj(o_nazwa)) << endl;
+    cout << "Minimum : " << minimum (wczytaj(o_nazwa)) << endl;
+    cout << "Srednia : " << srednia (wczytaj(o_nazwa)) <<  endl;
+    zapisz (wczytaj (o_nazwa), z_nazwa);
 }
